db6bb3deffebed896b69584390f0adcb165ffd6c
Patricio Villarreal Welsh <78720165+PatoVW02@users.noreply.github.com>
2024-05-14 23:41:50 -0600

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 // MongoDB connection URL
 const dbURL =
   process.env.MONGODB_URI ||
-  "mongodb+srv://a00834526:KXwo6175FNtI3VqB@cluster0.x40ar8w.mongodb.net/dev";
+  "mongodb+srv://*********:********@cluster0.x40ar8w.mongodb.net/dev";
 
 mongoose.connect(dbURL, {
   useNewUrlParser: true,
